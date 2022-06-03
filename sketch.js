a94312ff84ee49e7295c52bb522921a955af5c3f
jeffreyliu20 <90662821+jeffreyliu20@users.noreply.github.com>
2022-06-03 11:05:54 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
+float cx, cy, theta, circolor;
 function setup() {
-  // put setup code here
+  createCanvas(400, 400);
+  background(255);
 }
 
 function draw() {
-  // put drawing code here
+  background(255);
+  cy = calcY(100, height/2, theta);
+  cx = calcX(100, width/2, theta);
+  if (theta % 60==0) {
+    circolor+=10;
+  }
+  if (circolor>=255) {
+    circolor=0;
+  }
+  fill (0, circolor, 0);
+  circle(cx, cy, 50);
+  line(width/2,height/2,cx,cy);
+  theta++;
+}
+
+function calcY( amp, offset, t) {
+  var y;
+  y=sin(radians(t));
+  y=amp*y+offset;
+  return y;
+}
+
+function calcX(amp, offset, t) {
+  var x;
+  x=cos(radians(t));
+  x=amp*x+offset;
+  return x;
 }
